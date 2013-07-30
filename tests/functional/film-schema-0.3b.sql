d161f4c535da8beabd4f7a0a721ac60c15de5bf9
Joe Abbate <jma@freedomcircle.com>
2013-07-30 14:58:41 -0400

    Change film functional tests to use 'genre' instead of 'category'.

--- tests/functional/film-schema-0.3b.sql
+++ tests/functional/film-schema-0.3b.sql
@@ -1,12 +1,11 @@
-CREATE TABLE category (
-    category_id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(25) NOT NULL,
-    last_update TIMESTAMP WITH TIME ZONE NOT NULL
+CREATE TABLE genre (
+    genre_id INTEGER PRIMARY KEY,
+    name VARCHAR(25) NOT NULL
 );
-CREATE TABLE film_category (
+CREATE TABLE film_genre (
     film_id INTEGER NOT NULL REFERENCES film (id),
-    category_id INTEGER NOT NULL
-        REFERENCES category (category_id),
+    genre_id INTEGER NOT NULL
+        REFERENCES genre (genre_id),
     last_update TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY (film_id, category_id)
+    PRIMARY KEY (film_id, genre_id)
 );
