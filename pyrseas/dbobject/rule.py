4907a9361f17919a865c2310e232b8b8b6989354
Joe Abbate <jma@freedomcircle.com>
2015-03-12 15:37:38 -0400

    Changes to have rule tests pass with deptrack changes.

--- pyrseas/dbobject/rule.py
+++ pyrseas/dbobject/rule.py
@@ -7,7 +7,7 @@
     DbSchemaObject and DbObjectDict, respectively.
 """
 from pyrseas.dbobject import DbObjectDict, DbSchemaObject
-from pyrseas.dbobject import quote_id, commentable
+from pyrseas.dbobject import quote_id, commentable, split_schema_obj
 
 
 class Rule(DbSchemaObject):
@@ -45,6 +45,11 @@ class Rule(DbSchemaObject):
         return ["CREATE RULE %s AS ON %s\n    TO %s%s\n    DO %s%s" % (
                 quote_id(self.name), self.event.upper(),
                 self._table.qualname(), where, instead, self.actions)]
+
+    def get_implied_deps(self, db):
+        deps = super(Rule, self).get_implied_deps(db)
+        deps.add(db.tables[split_schema_obj(self.table, self.schema)])
+        return deps
 
 
 class RuleDict(DbObjectDict):
@@ -93,43 +98,3 @@ class RuleDict(DbObjectDict):
                 if 'description' in inrule:
                     rule.description = inrule['description']
             self[(table.schema, table.name, rul)] = rule
-
-    # TODO: drop and refactor
-    def diff_map(self, inrules):
-        """Generate SQL to transform existing rules
-
-        :param input_map: a YAML map defining the new rules
-        :return: list of SQL statements
-
-        Compares the existing rule definitions, as fetched from the
-        catalogs, to the input map and generates SQL statements to
-        transform the rules accordingly.
-        """
-        stmts = []
-        # check input rules
-        for rul in inrules:
-            inrul = inrules[rul]
-            # does it exist in the database?
-            if rul in self:
-                stmts.append(self[rul].diff_map(inrul))
-            else:
-                # check for possible RENAME
-                if hasattr(inrul, 'oldname'):
-                    oldname = inrul.oldname
-                    try:
-                        stmts.append(self[oldname].rename(inrul.name))
-                        del self[oldname]
-                    except KeyError as exc:
-                        exc.args = ("Previous name '%s' for rule '%s' "
-                                    "not found" % (oldname, inrul.name), )
-                        raise
-                else:
-                    # create new rule
-                    stmts.append(inrul.create())
-        # check database rules
-        for (sch, tbl, rul) in self:
-            # if missing, drop it
-            if (sch, tbl, rul) not in inrules:
-                stmts.append(self[(sch, tbl, rul)].drop())
-
-        return stmts
