perseas/Pyrseas

commit ffce721d3be5c3a9f7cb21b20e3b4a3989c3cdd7

Fix a couple of the problems with test_pagila.py.

pyrseas/dbobject/table.py
@@ -509,9 +509,10 @@ class Table(DbClass):
                     seq = db.tables.find(m.group(1), self.schema)
                     if seq:
                         deps.add(seq)
-                        if not hasattr(self, '_owned_seqs'):
-                            self._owned_seqs = []
-                        self._owned_seqs.append(seq)
+                        if hasattr(seq, 'owned_table'):
+                            if not hasattr(self, '_owned_seqs'):
+                                self._owned_seqs = []
+                            self._owned_seqs.append(seq)
 
         for pname in getattr(self, 'inherits', ()):
             parent = db.tables.find(pname, self.schema)

pyrseas/dbobject/__init__.py
@@ -76,7 +76,8 @@ def split_schema_obj(obj, sch=None):
         else:
             obj = obj[1:-1]
     else:
-        if '.' in obj:
+        # TODO: properly handle functions
+        if '.' in obj and not '(' in obj:
             (qualsch, obj) = obj.split('.')
     if sch != qualsch:
         sch = qualsch

pyrseas/dbobject/trigger.py
@@ -76,7 +76,8 @@ class Trigger(DbSchemaObject):
         # (another case for a robust lookup function in db)
         fschema, fname = split_schema_obj(self.procedure, self.schema)
         fname, _ = fname.split('(', 1) # implicitly assert there is a (
-        deps.add(db.functions[fschema, fname, ''])
+        if not fname.startswith('tsvector_update_trigger'):
+            deps.add(db.functions[fschema, fname, ''])
 
         return deps
 

commit 96aa3ffc0565d707e6b40b40ba766c059b2a25a2

Fix bug introduced by ffce721 in Table.get_implied_deps.

pyrseas/dbobject/table.py
@@ -509,7 +509,7 @@ class Table(DbClass):
                     seq = db.tables.find(m.group(1), self.schema)
                     if seq:
                         deps.add(seq)
-                        if hasattr(seq, 'owned_table'):
+                        if hasattr(seq, 'owner_table'):
                             if not hasattr(self, '_owned_seqs'):
                                 self._owned_seqs = []
                             self._owned_seqs.append(seq)