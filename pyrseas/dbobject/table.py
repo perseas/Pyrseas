perseas/Pyrseas

commit 5da6b0c6d39326319a8415430916b47cfafae5bf

First pass at adding declared attributes for all DbObject's.
   Add name, description, owner and privileges attributes to DbObject
   and schema attribute to DbSchemaObject.

   See issue #113.

pyrseas/dbobject/table.py
@@ -115,7 +115,8 @@ class Sequence(DbClass):
         for key, val in list(self.__dict__.items()):
             if key in self.keylist or key == 'dependent_table' or (
                     key == 'owner' and opts.no_owner) or (
-                    key == 'privileges' and opts.no_privs):
+                    key == 'privileges' and opts.no_privs) or (
+                    key == 'description' and self.description is None):
                 continue
             if key == 'privileges':
                 seq[key] = self.map_privs()
@@ -195,8 +196,8 @@ class Sequence(DbClass):
         if stmt:
             stmts.append("ALTER SEQUENCE %s" % self.qualname() + stmt)
 
-        if hasattr(inseq, 'owner'):
-            if hasattr(self, 'owner') and inseq.owner != self.owner:
+        if inseq.owner is not None:
+            if self.owner is not None and inseq.owner != self.owner:
                 stmts.append(self.alter_owner(inseq.owner))
         stmts.append(self.diff_privileges(inseq))
         stmts.append(self.diff_description(inseq))
@@ -242,7 +243,9 @@ class Table(DbClass):
             if col:
                 cols.append(col)
         tbl = {'columns': cols}
-        attrlist = ['description', 'options', 'tablespace', 'unlogged']
+        attrlist = ['options', 'tablespace', 'unlogged']
+        if self.description is not None:
+            attrlist.append('description')
         if not opts.no_owner:
             attrlist.append('owner')
         for attr in attrlist:
@@ -291,7 +294,7 @@ class Table(DbClass):
             for k in list(self.triggers.values()):
                 tbl['triggers'].update(self.triggers[k.name].to_map())
 
-        if not opts.no_privs and hasattr(self, 'privileges'):
+        if not opts.no_privs and self.privileges:
             tbl.update({'privileges': self.map_privs()})
 
         return tbl
@@ -325,17 +328,16 @@ class Table(DbClass):
         stmts.append("CREATE %sTABLE %s (\n%s)%s%s%s" % (
             unlogged, self.qualname(), ",\n".join(cols), inhclause, opts,
             tblspc))
-        if hasattr(self, 'owner'):
+        if self.owner is not None:
             stmts.append(self.alter_owner())
-        if hasattr(self, 'privileges'):
-            for priv in self.privileges:
-                stmts.append(add_grant(self, priv))
+        for priv in self.privileges:
+            stmts.append(add_grant(self, priv))
         if colprivs:
             stmts.append(colprivs)
-        if hasattr(self, 'description'):
+        if self.description is not None:
             stmts.append(self.comment())
         for col in self.columns:
-            if hasattr(col, 'description'):
+            if col.description is not None:
                 stmts.append(col.comment())
         self.created = True
         return stmts
@@ -437,7 +439,7 @@ class Table(DbClass):
         if diff_opts:
             stmts.append("ALTER %s %s %s" % (self.objtype, self.identifier(),
                                              diff_opts))
-        if hasattr(intable, 'owner'):
+        if intable.owner is not None:
             if intable.owner != self.owner:
                 stmts.append(self.alter_owner(intable.owner))
         stmts.append(self.diff_privileges(intable))
@@ -547,7 +549,7 @@ class View(DbClass):
         stmts = []
         if self.definition != inview.definition:
             stmts.append(self.create(inview.definition))
-        if hasattr(inview, 'owner'):
+        if inview.owner is not None:
             if inview.owner != self.owner:
                 stmts.append(self.alter_owner(inview.owner))
         stmts.append(self.diff_privileges(inview))
@@ -607,7 +609,7 @@ class MaterializedView(View):
         stmts = []
         if self.definition != inview.definition:
             stmts.append(self.create(inview.definition))
-        if hasattr(inview, 'owner'):
+        if inview.owner is not None:
             if inview.owner != self.owner:
                 stmts.append(self.alter_owner(inview.owner))
         stmts.append(self.diff_privileges(inview))
@@ -688,8 +690,6 @@ class ClassDict(DbObjectDict):
             self.query = QUERY_PRE93
         for table in self.fetch():
             sch, tbl = table.key()
-            if hasattr(table, 'privileges'):
-                table.privileges = table.privileges.split(',')
             if hasattr(table, 'persistence'):
                 if table.persistence == 'u':
                     table.unlogged = True
@@ -783,7 +783,7 @@ class ClassDict(DbObjectDict):
                 raise KeyError("Unrecognized object type: %s" % k)
             obj = self[(schema.name, key)]
             if 'privileges' in inobj:
-                    if not hasattr(obj, 'owner'):
+                    if obj.owner is None:
                         raise ValueError("%s '%s' has privileges but no "
                                          "owner information" %
                                          obj.objtype.capital(), table.name)

pyrseas/database.py
@@ -124,6 +124,7 @@ class Database(object):
 
     def _link_refs(self, db):
         """Link related objects"""
+        langs = []
         if self.dbconn.version >= 90100:
             langs = [lang[0] for lang in self.dbconn.fetchall(
                 """SELECT lanname FROM pg_language l

pyrseas/dbobject/__init__.py
@@ -82,7 +82,7 @@ def commentable(func):
     @wraps(func)
     def add_comment(obj, *args, **kwargs):
         stmts = func(obj, *args, **kwargs)
-        if hasattr(obj, 'description'):
+        if obj.description is not None:
             stmts.append(obj.comment())
         return stmts
     return add_comment
@@ -93,9 +93,8 @@ def grantable(func):
     @wraps(func)
     def grant(obj, *args, **kwargs):
         stmts = func(obj, *args, **kwargs)
-        if hasattr(obj, 'privileges'):
-            for priv in obj.privileges:
-                stmts.append(add_grant(obj, priv))
+        for priv in obj.privileges:
+            stmts.append(add_grant(obj, priv))
         return stmts
     return grant
 
@@ -105,8 +104,7 @@ def ownable(func):
     @wraps(func)
     def add_alter(obj, *args, **kwargs):
         stmts = func(obj, *args, **kwargs)
-        if hasattr(obj, 'owner'):
-            stmts.append(obj.alter_owner())
+        stmts.append(obj.alter_owner())
         return stmts
     return add_alter
 
@@ -129,15 +127,26 @@ class DbObject(object):
 
     allprivs = ''
 
-    def __init__(self, **attrs):
+    def __init__(self, name=None, description=None, owner=None,
+                 privileges=None, **attrs):
         """Initialize the catalog object from a dictionary of attributes
 
-        :param attrs: the dictionary of attributes
+        :param name: name of object
+        :param description: comment text describing object
+        :param owner: name of user that owns the object
+        :param privileges: privileges on object
+        :param attrs: dictionary of other attributes
 
         Non-key attributes without a value are discarded. Values that
         are multi-line strings are treated specially so that YAML will
         output them in block style.
         """
+        self.name = name
+        self.description = description
+        self.owner = owner
+        if isinstance(privileges, strtypes):
+            privileges = privileges.split(',')
+        self.privileges = privileges or []
         for key, val in list(attrs.items()):
             if val or key in self.keylist:
                 if key in ['definition', 'description', 'source'] and \
@@ -262,13 +271,14 @@ class DbObject(object):
         dct = self.__dict__.copy()
         for key in self.keylist:
             del dct[key]
-        if no_owner and hasattr(self, 'owner'):
+        if self.description is None:
+            del dct['description']
+        if no_owner or self.owner is None:
             del dct['owner']
-        if hasattr(self, 'privileges'):
-            if no_privs:
-                del dct['privileges']
-            else:
-                dct['privileges'] = self.map_privs()
+        if len(self.privileges) == 0 or no_privs:
+            del dct['privileges']
+        else:
+            dct['privileges'] = self.map_privs()
         return dct
 
     def to_map(self, no_owner=False, no_privs=False):
@@ -291,9 +301,8 @@ class DbObject(object):
         :return: list
         """
         privlist = []
-        owner = self.owner if hasattr(self, 'owner') else ''
         for prv in self.privileges:
-            privlist.append(privileges_to_map(prv, self.allprivs, owner))
+            privlist.append(privileges_to_map(prv, self.allprivs, self.owner))
         return privlist
 
     def _comment_text(self):
@@ -301,7 +310,7 @@ class DbObject(object):
 
         :return: string
         """
-        if hasattr(self, 'description'):
+        if self.description is not None:
             return "'%s'" % self.description.replace("'", "''")
         else:
             return 'NULL'
@@ -319,8 +328,11 @@ class DbObject(object):
 
         :return: SQL statement
         """
-        return "ALTER %s %s OWNER TO %s" % (
-            self.objtype, self.identifier(), owner or self.owner)
+        if self.owner != owner:
+            return "ALTER %s %s OWNER TO %s" % (
+                self.objtype, self.identifier(), owner or self.owner)
+        else:
+            return []
 
     def drop(self):
         """Return SQL statement to DROP the object
@@ -350,7 +362,7 @@ class DbObject(object):
         comments.
         """
         stmts = []
-        if not no_owner and hasattr(inobj, 'owner') and hasattr(self, 'owner'):
+        if not no_owner and self.owner is not None and inobj.owner is not None:
             if inobj.owner != self.owner:
                 stmts.append(self.alter_owner(inobj.owner))
         stmts.append(self.diff_privileges(inobj))
@@ -363,11 +375,7 @@ class DbObject(object):
         :param inobj: a YAML map defining the input object
         :return: list of SQL statements
         """
-        stmts = []
-        currprivs = self.privileges if hasattr(self, 'privileges') else {}
-        newprivs = inobj.privileges if hasattr(inobj, 'privileges') else {}
-        stmts.append(diff_privs(self, currprivs, inobj, newprivs))
-        return stmts
+        return diff_privs(self, self.privileges, inobj, inobj.privileges)
 
     def diff_description(self, inobj):
         """Generate SQL statements to add or change COMMENTs
@@ -376,16 +384,16 @@ class DbObject(object):
         :return: list of SQL statements
         """
         stmts = []
-        if hasattr(self, 'description'):
-            if hasattr(inobj, 'description'):
+        if self.description is not None:
+            if inobj.description is not None:
                 if self.description != inobj.description:
                     self.description = inobj.description
                     stmts.append(self.comment())
             else:
-                del self.description
+                self.description = None
                 stmts.append(self.comment())
         else:
-            if hasattr(inobj, 'description'):
+            if inobj.description is not None:
                 self.description = inobj.description
                 stmts.append(self.comment())
         return stmts
@@ -393,6 +401,21 @@ class DbObject(object):
 
 class DbSchemaObject(DbObject):
     "A database object that is owned by a certain schema"
+
+    def __init__(self, schema, name, description=None, owner=None,
+                 privileges=None, **attrs):
+        """Initialize the catalog object from a dictionary of attributes
+
+        :param schema: name of the schema owning the object
+        :param name: name of object
+        :param description: comment text describing object
+        :param owner: name of user that owns the object
+        :param privileges: privileges on object
+        :param attrs: dictionary of other attributes
+        """
+        self.schema = schema
+        super(DbSchemaObject, self).__init__(name, description, owner,
+                                             privileges, **attrs)
 
     def identifier(self):
         """Return a full identifier for a schema object
@@ -477,8 +500,6 @@ class DbObjectDict(dict):
         This is may be overriden by derived classes as needed.
         """
         for obj in self.fetch():
-            if hasattr(obj, 'privileges'):
-                obj.privileges = obj.privileges.split(',')
             self[obj.key()] = obj
 
     def to_map(self,  opts):

pyrseas/dbobject/cast.py
@@ -41,6 +41,7 @@ class Cast(DbObject):
         :return: dictionary
         """
         dct = self._base_map()
+        del dct['name']
         dct['context'] = CONTEXTS[self.context]
         dct['method'] = METHODS[self.method]
         return dct

pyrseas/dbobject/column.py
@@ -52,19 +52,15 @@ class Column(DbSchemaObject):
                 stmt += ' DEFAULT ' + self.default
         if hasattr(self, 'collation') and self.collation != 'default':
             stmt += ' COLLATE "%s"' % self.collation
-        return (stmt, '' if not hasattr(self, 'description')
-                else self.comment())
+        return (stmt, '' if self.description is None else self.comment())
 
     def add_privs(self):
         """Generate SQL statements to grant privileges on new column
 
         :return: list of SQL statements
         """
-        stmts = []
-        if hasattr(self, 'privileges'):
-            for priv in self.privileges:
-                stmts.append(add_grant(self._table, priv, self.name))
-        return stmts
+        return [add_grant(self._table, priv, self.name)
+                for priv in self.privileges]
 
     def diff_privileges(self, incol):
         """Generate SQL statements to grant or revoke privileges
@@ -72,12 +68,8 @@ class Column(DbSchemaObject):
         :param incol: a YAML map defining the input column
         :return: list of SQL statements
         """
-        stmts = []
-        currprivs = self.privileges if hasattr(self, 'privileges') else {}
-        newprivs = incol.privileges if hasattr(incol, 'privileges') else {}
-        stmts.append(diff_privs(self._table, currprivs, incol._table, newprivs,
-                                self.name))
-        return stmts
+        return [diff_privs(self._table, self.privileges, incol._table,
+                           incol.privileges, self.name)]
 
     def comment(self):
         """Return a SQL COMMENT statement for the column
@@ -227,8 +219,6 @@ class ColumnDict(DbObjectDict):
         if self.dbconn.version < 90100:
             self.query = QUERY_PRE91
         for col in self.fetch():
-            if hasattr(col, 'privileges'):
-                col.privileges = col.privileges.split(',')
             sch, tbl = col.key()
             if (sch, tbl) not in self:
                 self[(sch, tbl)] = []
@@ -252,8 +242,8 @@ class ColumnDict(DbObjectDict):
                     arg = {'type': incol[key]}
                 col = Column(schema=table.schema, table=table.name, name=key,
                              **arg)
-                if hasattr(col, 'privileges'):
-                    if not hasattr(table, 'owner'):
+                if len(col.privileges) > 0:
+                    if table.owner is None:
                         raise ValueError("Column '%s.%s' has privileges but "
                                          "no owner information" % (
                                          table.name, key))

pyrseas/dbobject/dbtype.py
@@ -106,9 +106,9 @@ class Composite(DbType):
             if att:
                 attrs.append(att)
         dct = {'attributes': attrs}
-        if not no_owner and hasattr(self, 'owner'):
+        if not no_owner and self.owner is not None:
             dct.update(owner=self.owner)
-        if hasattr(self, 'description'):
+        if self.description is not None:
             dct.update(description=self.description)
         return dct
 
@@ -162,7 +162,7 @@ class Composite(DbType):
                 if descr:
                     stmts.append(descr)
 
-        if hasattr(intype, 'owner'):
+        if intype.owner is not None:
             if intype.owner != self.owner:
                 stmts.append(self.alter_owner(intype.owner))
         stmts.append(self.diff_description(intype))

pyrseas/dbobject/foreign.py
@@ -126,7 +126,7 @@ class ForeignDataWrapper(DbObjectWithOptions):
         :return: list of SQL statements
         """
         stmts = super(ForeignDataWrapper, self).diff_map(inwrapper)
-        if hasattr(inwrapper, 'owner'):
+        if inwrapper.owner is not None:
             if inwrapper.owner != self.owner:
                 stmts.append(self.alter_owner(inwrapper.owner))
         stmts.append(self.diff_description(inwrapper))
@@ -320,7 +320,7 @@ class ForeignServer(DbObjectWithOptions):
         :return: list of SQL statements
         """
         stmts = super(ForeignServer, self).diff_map(inserver)
-        if hasattr(inserver, 'owner'):
+        if inserver.owner is not None:
             if inserver.owner != self.owner:
                 stmts.append(self.alter_owner(inserver.owner))
         stmts.append(self.diff_description(inserver))
@@ -449,14 +449,14 @@ class UserMapping(DbObjectWithOptions):
 
     objtype = "USER MAPPING"
 
-    keylist = ['wrapper', 'server', 'username']
+    keylist = ['wrapper', 'server', 'name']
 
     def extern_key(self):
         """Return the key to be used in external maps for this user mapping
 
         :return: string
         """
-        return self.username
+        return self.name
 
     def identifier(self):
         """Return a full identifier for a user mapping object
@@ -464,7 +464,7 @@ class UserMapping(DbObjectWithOptions):
         :return: string
         """
         return "FOR %s SERVER %s" % (
-            self.username == 'PUBLIC' and 'PUBLIC' or quote_id(self.username),
+            self.name == 'PUBLIC' and 'PUBLIC' or quote_id(self.name),
             quote_id(self.server))
 
     def create(self):
@@ -476,8 +476,8 @@ class UserMapping(DbObjectWithOptions):
         if hasattr(self, 'options'):
             options.append(self.options_clause())
         return ["CREATE USER MAPPING FOR %s\n    SERVER %s%s" % (
-                self.username == 'PUBLIC' and 'PUBLIC' or
-                quote_id(self.username), quote_id(self.server),
+                self.name == 'PUBLIC' and 'PUBLIC' or
+                quote_id(self.name), quote_id(self.server),
                 options and '\n    ' + ',\n    '.join(options) or '')]
 
 
@@ -488,7 +488,7 @@ class UserMappingDict(DbObjectDict):
     query = \
         """SELECT fdwname AS wrapper, s.srvname AS server,
                   CASE umuser WHEN 0 THEN 'PUBLIC' ELSE
-                  usename END AS username, umoptions AS options
+                  usename END AS name, umoptions AS options
            FROM pg_user_mappings u
                 JOIN pg_foreign_server s ON (u.srvid = s.oid)
                 JOIN pg_foreign_data_wrapper w ON (srvfdw = w.oid)
@@ -502,7 +502,7 @@ class UserMappingDict(DbObjectDict):
         """
         for key in inusermaps:
             usermap = UserMapping(wrapper=server.wrapper, server=server.name,
-                                  username=key)
+                                  name=key)
             inusermap = inusermaps[key]
             if inusermap:
                 for attr, val in list(inusermap.items()):
@@ -587,13 +587,15 @@ class ForeignTable(DbObjectWithOptions, Table):
             if col:
                 cols.append(col)
         tbl = {'columns': cols, 'server': self.server}
-        attrlist = ['options', 'description']
+        attrlist = ['options']
+        if self.description is not None:
+            attrlist.append('description')
         if not opts.no_owner:
             attrlist.append('owner')
         for attr in attrlist:
             if hasattr(self, attr):
                 tbl.update({attr: getattr(self, attr)})
-        if not opts.no_privs and hasattr(self, 'privileges'):
+        if not opts.no_privs and self.privileges:
             tbl.update({'privileges': self.map_privs()})
 
         return tbl
@@ -614,12 +616,12 @@ class ForeignTable(DbObjectWithOptions, Table):
         stmts.append("CREATE FOREIGN TABLE %s (\n%s)\n    SERVER %s%s" % (
             self.qualname(), ",\n".join(cols), self.server,
             options and '\n    ' + ',\n    '.join(options) or ''))
-        if hasattr(self, 'owner'):
+        if self.owner is not None:
             stmts.append(self.alter_owner())
-        if hasattr(self, 'description'):
+        if self.description is not None:
             stmts.append(self.comment())
         for col in self.columns:
-            if hasattr(col, 'description'):
+            if col.description is not None:
                 stmts.append(col.comment())
         return stmts
 
@@ -637,7 +639,7 @@ class ForeignTable(DbObjectWithOptions, Table):
         :return: list of SQL statements
         """
         stmts = super(ForeignTable, self).diff_map(intable)
-        if hasattr(intable, 'owner'):
+        if intable.owner is not None:
             if intable.owner != self.owner:
                 stmts.append(self.alter_owner(intable.owner))
         stmts.append(self.diff_description(intable))
@@ -667,8 +669,6 @@ class ForeignTableDict(ClassDict):
         if self.dbconn.version < 90100:
             return
         for tbl in self.fetch():
-            if hasattr(tbl, 'privileges'):
-                tbl.privileges = tbl.privileges.split(',')
             self[tbl.key()] = tbl
 
     def from_map(self, schema, inobjs, newdb):

pyrseas/dbobject/function.py
@@ -51,7 +51,7 @@ class Function(Proc):
         :param no_privs: exclude privilege information
         :return: dictionary
         """
-        dct = self._base_map(no_owner)
+        dct = self._base_map(no_owner, no_privs)
         if self.volatility == 'v':
             del dct['volatility']
         else:
@@ -73,11 +73,6 @@ class Function(Proc):
         if hasattr(self, 'rows') and self.rows != 0:
             if self.rows == 1000:
                 del dct['rows']
-        if hasattr(self, 'privileges'):
-            if no_privs:
-                del dct['privileges']
-            else:
-                dct['privileges'] = self.map_privs()
 
         return dct
 
@@ -147,7 +142,7 @@ class Function(Proc):
         if hasattr(self, 'source') and hasattr(infunction, 'source'):
             if self.source != infunction.source:
                 stmts.append(self.create(infunction.source))
-        if hasattr(infunction, 'owner'):
+        if infunction.owner is not None:
             if infunction.owner != self.owner:
                 stmts.append(self.alter_owner(infunction.owner))
         if hasattr(self, 'leakproof') and self.leakproof is True:
@@ -264,8 +259,6 @@ class ProcDict(DbObjectDict):
         if self.dbconn.version < 90200:
             self.query = QUERY_PRE92
         for proc in self.fetch():
-            if hasattr(proc, 'privileges'):
-                proc.privileges = proc.privileges.split(',')
             sch, prc, arg = proc.key()
             if hasattr(proc, 'allargs') and proc.allargs == proc.arguments:
                 del proc.allargs

commit c58e914668fc6263682ebb8399a150feaec98af0

Fix problem introduced by 5da6b0c, causing sequence empty privs to show as [].

pyrseas/dbobject/table.py
@@ -119,7 +119,9 @@ class Sequence(DbClass):
                     key == 'description' and self.description is None):
                 continue
             if key == 'privileges':
-                seq[key] = self.map_privs()
+                privs = self.map_privs()
+                if privs != []:
+                    seq[key] = privs
             elif key == 'max_value' and val == MAX_BIGINT:
                 seq[key] = None
             elif key == 'min_value' and val == 1: