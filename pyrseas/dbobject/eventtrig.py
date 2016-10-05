7a81124c8660e121991c54f9f3885fd7f0289abb
Joe Abbate <jma@freedomcircle.com>
2016-10-05 15:14:18 -0400

    Exclude event triggers installed by extensions. Fixes #151.

--- pyrseas/dbobject/eventtrig.py
+++ pyrseas/dbobject/eventtrig.py
@@ -44,6 +44,7 @@ class EventTriggerDict(DbObjectDict):
                   obj_description(t.oid, 'pg_event_trigger') AS description
            FROM pg_event_trigger t
                 JOIN pg_roles ON (evtowner = pg_roles.oid)
+           WHERE t.oid NOT IN (SELECT objid FROM pg_depend WHERE deptype = 'e')
            ORDER BY 1"""
     enable_modes = {'O': True, 'D': False, 'R': 'replica',
                     'A': 'always'}
