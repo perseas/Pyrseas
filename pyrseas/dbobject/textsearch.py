8b19b1d45dfa593d8870efeea13781a381d97414
Joe Abbate <jmafc@users.noreply.github.com>
2017-09-25 21:57:52 -0400

    Create proper constructor for text search objects. See issue #113

--- pyrseas/dbobject/textsearch.py
+++ pyrseas/dbobject/textsearch.py
@@ -8,8 +8,8 @@
     TSConfigurationDict, TSDictionaryDict, TSParserDict and
     TSTemplateDict derived from DbObjectDict.
 """
-from pyrseas.dbobject import DbObjectDict, DbSchemaObject
-from pyrseas.dbobject import commentable, ownable, split_schema_obj
+from . import DbObjectDict, DbSchemaObject
+from . import commentable, ownable, split_schema_obj
 
 
 class TSConfiguration(DbSchemaObject):
@@ -18,6 +18,51 @@ class TSConfiguration(DbSchemaObject):
     keylist = ['schema', 'name']
     single_extern_file = True
     catalog = 'pg_ts_config'
+
+    def __init__(self, name, schema, description, owner, parser,
+                 oid=None):
+        """Initialize the configuration
+
+        :param name: configuration name (from cfgname)
+        :param description: comment text (from obj_description())
+        :param schema: schema name (from cfgnamespace)
+        :param owner: owner name (from rolname via cfgowner)
+        :param parser: parser name (from prsname via cfgparser)
+        """
+        super(TSConfiguration, self).__init__(name, schema, description)
+        self._init_own_privs(owner, [])
+        self.parser = parser
+        self.oid = oid
+
+    @staticmethod
+    def query():
+        return """
+            SELECT nc.nspname AS schema, cfgname AS name,
+                   rolname AS owner, np.nspname || '.' || prsname AS parser,
+                   obj_description(c.oid, 'pg_ts_config') AS description, c.oid
+            FROM pg_ts_config c JOIN pg_roles r ON (r.oid = cfgowner)
+                 JOIN pg_ts_parser p ON (cfgparser = p.oid)
+                 JOIN pg_namespace nc ON (cfgnamespace = nc.oid)
+                 JOIN pg_namespace np ON (prsnamespace = np.oid)
+            WHERE nc.nspname != 'pg_catalog'
+              AND nc.nspname != 'information_schema'
+            ORDER BY nc.nspname, cfgname"""
+
+    @staticmethod
+    def from_map(name, schema, inobj):
+        """Initialize a configuration instance from a YAML map
+
+        :param name: configuration name
+        :param name: schema map
+        :param inobj: YAML map of the configuration
+        :return: configuration instance
+        """
+        tsc = TSConfiguration(
+            name, schema.name, inobj.pop('description', None),
+            inobj.pop('owner', None), inobj.pop('parser', None))
+        if 'oldname' in inobj:
+            tsc.oldname = inobj.get('oldname')
+        return tsc
 
     @property
     def objtype(self):
@@ -57,18 +102,6 @@ class TSConfigurationDict(DbObjectDict):
     "The collection of text search configurations in a database"
 
     cls = TSConfiguration
-    query = \
-        """SELECT c.oid, nc.nspname AS schema, cfgname AS name,
-                  rolname AS owner, np.nspname || '.' || prsname AS parser,
-                  obj_description(c.oid, 'pg_ts_config') AS description
-           FROM pg_ts_config c
-                JOIN pg_roles r ON (r.oid = cfgowner)
-                JOIN pg_ts_parser p ON (cfgparser = p.oid)
-                JOIN pg_namespace nc ON (cfgnamespace = nc.oid)
-                JOIN pg_namespace np ON (prsnamespace = np.oid)
-           WHERE (nc.nspname != 'pg_catalog'
-                  AND nc.nspname != 'information_schema')
-           ORDER BY nc.nspname, cfgname"""
 
     def from_map(self, schema, inconfigs):
         """Initialize the dictionary of configs by examining the input map
@@ -80,17 +113,9 @@ class TSConfigurationDict(DbObjectDict):
             if not key.startswith('text search configuration '):
                 raise KeyError("Unrecognized object type: %s" % key)
             tsc = key[26:]
-            self[(schema.name, tsc)] = config = TSConfiguration(
-                schema=schema.name, name=tsc)
-            inconfig = inconfigs[key]
-            if inconfig:
-                for attr, val in list(inconfig.items()):
-                    setattr(config, attr, val)
-                if 'oldname' in inconfig:
-                    config.oldname = inconfig['oldname']
-                    del inconfig['oldname']
-                if 'description' in inconfig:
-                    config.description = inconfig['description']
+            inobj = inconfigs[key]
+            self[(schema.name, tsc)] = TSConfiguration.from_map(
+                tsc, schema, inobj)
 
 
 class TSDictionary(DbSchemaObject):
@@ -99,6 +124,52 @@ class TSDictionary(DbSchemaObject):
     keylist = ['schema', 'name']
     single_extern_file = True
     catalog = 'pg_ts_dict'
+
+    def __init__(self, name, schema, description, owner, template, options,
+                 oid=None):
+        """Initialize the dictionary
+
+        :param name: dictionary name (from dictname)
+        :param schema: schema name (from dictnamespace)
+        :param description: comment text (from obj_description())
+        :param owner: owner name (from rolname via dictowner)
+        :param template: template name (from dicttemplate)
+        :param options: initialization option string (from dictinitoption)
+        """
+        super(TSDictionary, self).__init__(name, schema, description)
+        self._init_own_privs(owner, [])
+        self.template = template
+        self.options = options
+        self.oid = oid
+
+    @staticmethod
+    def query():
+        return """
+            SELECT nspname AS schema, dictname AS name, rolname AS owner,
+                   tmplname AS template, dictinitoption AS options,
+                   obj_description(d.oid, 'pg_ts_dict') AS description, d.oid
+            FROM pg_ts_dict d JOIN pg_ts_template t ON (dicttemplate = t.oid)
+                 JOIN pg_roles r ON (r.oid = dictowner)
+                 JOIN pg_namespace n ON (dictnamespace = n.oid)
+            WHERE nspname != 'pg_catalog' AND nspname != 'information_schema'
+            ORDER BY nspname, dictname"""
+
+    @staticmethod
+    def from_map(name, schema, inobj):
+        """Initialize a dictionary instance from a YAML map
+
+        :param name: dictionary name
+        :param name: schema map
+        :param inobj: YAML map of the dictionary
+        :return: dictionary instance
+        """
+        tsd = TSDictionary(
+            name, schema.name, inobj.pop('description', None),
+            inobj.pop('owner', None), inobj.pop('template', None),
+            inobj.pop('options', None))
+        if 'oldname' in inobj:
+            tsd.oldname = inobj.get('oldname')
+        return tsd
 
     @property
     def objtype(self):
@@ -113,7 +184,7 @@ class TSDictionary(DbSchemaObject):
         """
         clauses = []
         clauses.append("TEMPLATE = %s" % self.template)
-        if hasattr(self, 'options'):
+        if self.options is not None:
             clauses.append(self.options)
         return ["CREATE TEXT SEARCH DICTIONARY %s (\n    %s)" % (
                 self.qualname(), ',\n    '.join(clauses))]
@@ -123,15 +194,6 @@ class TSDictionaryDict(DbObjectDict):
     "The collection of text search dictionaries in a database"
 
     cls = TSDictionary
-    query = \
-        """SELECT d.oid, nspname AS schema, dictname AS name, rolname AS owner,
-                  tmplname AS template, dictinitoption AS options,
-                  obj_description(d.oid, 'pg_ts_dict') AS description
-           FROM pg_ts_dict d JOIN pg_ts_template t ON (dicttemplate = t.oid)
-                JOIN pg_roles r ON (r.oid = dictowner)
-                JOIN pg_namespace n ON (dictnamespace = n.oid)
-           WHERE (nspname != 'pg_catalog' AND nspname != 'information_schema')
-           ORDER BY nspname, dictname"""
 
     def from_map(self, schema, indicts):
         """Initialize the dictionary of dictionaries by examining the input map
@@ -143,17 +205,9 @@ class TSDictionaryDict(DbObjectDict):
             if not key.startswith('text search dictionary '):
                 raise KeyError("Unrecognized object type: %s" % key)
             tsd = key[23:]
-            self[(schema.name, tsd)] = tsdict = TSDictionary(
-                schema=schema.name, name=tsd)
-            indict = indicts[key]
-            if indict:
-                for attr, val in list(indict.items()):
-                    setattr(tsdict, attr, val)
-                if 'oldname' in indict:
-                    tsdict.oldname = indict['oldname']
-                    del indict['oldname']
-                if 'description' in indict:
-                    tsdict.description = indict['description']
+            inobj = indicts[key]
+            self[(schema.name, tsd)] = TSDictionary.from_map(
+                tsd, schema, inobj)
 
 
 class TSParser(DbSchemaObject):
@@ -162,6 +216,58 @@ class TSParser(DbSchemaObject):
     keylist = ['schema', 'name']
     single_extern_file = True
     catalog = 'pg_ts_parser'
+
+    def __init__(self, name, schema, description, start, gettoken, end,
+                 headline, lextypes,
+                 oid=None):
+        """Initialize the parser
+
+        :param name: parser name (from prsname)
+        :param schema: schema name (from prsnamespace)
+        :param description: comment text (from obj_description())
+        :param start: startup function (from prsstart)
+        :param gettoken: next-token function (from prstoken)
+        :param end: shutdown function (from prsend)
+        :param headline: headline function (from prsheadline)
+        :param lextypes: lextype function (from prslextype)
+        """
+        super(TSParser, self).__init__(name, schema, description)
+        self._init_own_privs(None, [])
+        self.start = start
+        self.gettoken = gettoken
+        self.end = end
+        self.headline = headline
+        self.lextypes = lextypes
+        self.oid = oid
+
+    @staticmethod
+    def query():
+        return """
+            SELECT nspname AS schema, prsname AS name,
+                   prsstart::regproc AS start, prstoken::regproc AS gettoken,
+                   prsend::regproc AS end, prslextype::regproc AS lextypes,
+                   prsheadline::regproc AS headline,
+                   obj_description(p.oid, 'pg_ts_parser') AS description, p.oid
+            FROM pg_ts_parser p JOIN pg_namespace n ON (prsnamespace = n.oid)
+            WHERE nspname != 'pg_catalog' AND nspname != 'information_schema'
+            ORDER BY nspname, prsname"""
+
+    @staticmethod
+    def from_map(name, schema, inobj):
+        """Initialize a parser instance from a YAML map
+
+        :param name: parser name
+        :param name: schema map
+        :param inobj: YAML map of the parser
+        :return: parser instance
+        """
+        tsp = TSParser(name, schema.name, inobj.pop('description', None),
+                       inobj.pop('start', None), inobj.pop('gettoken', None),
+                       inobj.pop('end', None), inobj.pop('headline', None),
+                       inobj.pop('lextypes', None))
+        if 'oldname' in inobj:
+            tsp.oldname = inobj.get('oldname')
+        return tsp
 
     @property
     def objtype(self):
@@ -177,7 +283,7 @@ class TSParser(DbSchemaObject):
         clauses = []
         for attr in ['start', 'gettoken', 'end', 'lextypes']:
             clauses.append("%s = %s" % (attr.upper(), getattr(self, attr)))
-        if hasattr(self, 'headline'):
+        if self.headline is not None:
             clauses.append("HEADLINE = %s" % self.headline)
         return ["CREATE TEXT SEARCH PARSER %s (\n    %s)" % (
                 self.qualname(), ',\n    '.join(clauses))]
@@ -187,16 +293,6 @@ class TSParserDict(DbObjectDict):
     "The collection of text search parsers in a database"
 
     cls = TSParser
-    query = \
-        """SELECT p.oid, nspname AS schema, prsname AS name,
-                  prsstart::regproc AS start, prstoken::regproc AS gettoken,
-                  prsend::regproc AS end, prslextype::regproc AS lextypes,
-                  prsheadline::regproc AS headline,
-                  obj_description(p.oid, 'pg_ts_parser') AS description
-           FROM pg_ts_parser p
-                JOIN pg_namespace n ON (prsnamespace = n.oid)
-           WHERE (nspname != 'pg_catalog' AND nspname != 'information_schema')
-           ORDER BY nspname, prsname"""
 
     def from_map(self, schema, inparsers):
         """Initialize the dictionary of parsers by examining the input map
@@ -208,17 +304,8 @@ class TSParserDict(DbObjectDict):
             if not key.startswith('text search parser '):
                 raise KeyError("Unrecognized object type: %s" % key)
             tsp = key[19:]
-            self[(schema.name, tsp)] = parser = TSParser(
-                schema=schema.name, name=tsp)
-            inparser = inparsers[key]
-            if inparser:
-                for attr, val in list(inparser.items()):
-                    setattr(parser, attr, val)
-                if 'oldname' in inparser:
-                    parser.oldname = inparser['oldname']
-                    del inparser['oldname']
-                if 'description' in inparser:
-                    parser.description = inparser['description']
+            inobj = inparsers[key]
+            self[(schema.name, tsp)] = TSParser.from_map(tsp, schema, inobj)
 
 
 class TSTemplate(DbSchemaObject):
@@ -227,6 +314,48 @@ class TSTemplate(DbSchemaObject):
     keylist = ['schema', 'name']
     single_extern_file = True
     catalog = 'pg_ts_template'
+
+    def __init__(self, name, schema, description, init, lexize,
+                 oid=None):
+        """Initialize the template
+
+        :param name: template name (from tmplname)
+        :param schema: schema name (from tmplnamespace)
+        :param description: comment text (from obj_description())
+        :param init: initialization function (from tmplinit)
+        :param lexize: lexize function (from tmpllexize)
+        """
+        super(TSTemplate, self).__init__(name, schema, description)
+        self._init_own_privs(None, [])
+        self.init = init
+        self.lexize = lexize
+        self.oid = oid
+
+    @staticmethod
+    def query():
+        return """
+            SELECT nspname AS schema, tmplname AS name, p.oid,
+                   tmplinit::regproc AS init, tmpllexize::regproc AS lexize,
+                   obj_description(p.oid, 'pg_ts_template') AS description
+            FROM pg_ts_template p
+                 JOIN pg_namespace n ON (tmplnamespace = n.oid)
+            WHERE nspname != 'pg_catalog' AND nspname != 'information_schema'
+            ORDER BY nspname, tmplname"""
+
+    @staticmethod
+    def from_map(name, schema, inobj):
+        """Initialize a template instance from a YAML map
+
+        :param name: template name
+        :param name: schema map
+        :param inobj: YAML map of the template
+        :return: template instance
+        """
+        tst = TSTemplate(name, schema.name, inobj.pop('description', None),
+                         inobj.pop('init', None), inobj.pop('lexize', None))
+        if 'oldname' in inobj:
+            tst.oldname = inobj.get('oldname')
+        return tst
 
     @property
     def objtype(self):
@@ -239,7 +368,7 @@ class TSTemplate(DbSchemaObject):
         :return: SQL statements
         """
         clauses = []
-        if hasattr(self, 'init'):
+        if self.init is not None:
             clauses.append("INIT = %s" % self.init)
         clauses.append("LEXIZE = %s" % self.lexize)
         return ["CREATE TEXT SEARCH TEMPLATE %s (\n    %s)" % (
@@ -250,14 +379,6 @@ class TSTemplateDict(DbObjectDict):
     "The collection of text search templates in a database"
 
     cls = TSTemplate
-    query = \
-        """SELECT p.oid, nspname AS schema, tmplname AS name,
-                  tmplinit::regproc AS init, tmpllexize::regproc AS lexize,
-                  obj_description(p.oid, 'pg_ts_template') AS description
-           FROM pg_ts_template p
-                JOIN pg_namespace n ON (tmplnamespace = n.oid)
-           WHERE (nspname != 'pg_catalog' AND nspname != 'information_schema')
-           ORDER BY nspname, tmplname"""
 
     def from_map(self, schema, intemplates):
         """Initialize the dictionary of templates by examining the input map
@@ -269,14 +390,6 @@ class TSTemplateDict(DbObjectDict):
             if not key.startswith('text search template '):
                 raise KeyError("Unrecognized object type: %s" % key)
             tst = key[21:]
-            self[(schema.name, tst)] = template = TSTemplate(
-                schema=schema.name, name=tst)
-            intemplate = intemplates[key]
-            if intemplate:
-                for attr, val in list(intemplate.items()):
-                    setattr(template, attr, val)
-                if 'oldname' in intemplate:
-                    template.oldname = intemplate['oldname']
-                    del intemplate['oldname']
-                if 'description' in intemplate:
-                    template.description = intemplate['description']
+            inobj = intemplates[key]
+            self[(schema.name, tst)] = TSTemplate.from_map(
+                tst, schema, inobj)
